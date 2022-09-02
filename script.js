f0f15093ceff6dc2a4fcf848ee1cd8d6012a3fec
Keeks-cloud <112364744+Keeks-cloud@users.noreply.github.com>
2022-09-02 13:10:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
+//Interest Rate Slider
+function updateRate()
+{
+var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+//Compute Button
+function compute()
+{var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var principal = document.getElementById("principal").value;
+document.getElementById("results").innerHTML=
+    "If you deposit <mark>"+principal+
+    "</mark>,<br>at an interest rate of <mark> "+rate+
+    "% </mark>,<br>You will receive an amount of <mark> " +(principal * rate* years)/100+
+    "</mark>,<br> in the year <mark>"+year;
+  
+//Alert and focus
+    if (principal<1)
+    {
+        alert("Enter a positive number");
+       document.getElementById("principal").focus();
 
-function compute(){
-  var principal = document.getElementById("principal").value;
-  var rate = document.getElementById("rate").value;
-  var years = document.getElementById("years").value;
-  var interest = principal * years * rate /100;
-  var year = new Date().getFullYear()+parseInt(years);
-      if (principal==5){
-       return ("hello")
-      }
-//      alert("Enter a positive number");
-//          principal.focus();
-//         return ("test");
-//function updateRate()
-//{
-  //  var rateval = document.getElementById("rate").value;
-    //document.getElementById("rate_val").innerText=rateval;
-//}
-//function onchange(updateRate);
-//{
-//}
-
-//function compute()
-//{
- //   document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of <mark>"+rate+"%\<br\></mark>You will receive an amount of <mark> "+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
-//}
-
-    
-//}
+    }
+}
